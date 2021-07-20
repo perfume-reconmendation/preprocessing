4f771138922e3852483104a4c65b1a26b904cbb8
KYUUUW <duffufk1502@gmail.com>
2021-07-21 02:41:02 +0900

    Remove orthography

--- src/processor.py
+++ src/processor.py
@@ -83,7 +83,7 @@ def preprocessing(doc, stopwords={' '}):
     words = map(trim, words)
     words = [w for w in words if len(w) > 2]
     words = remove_stopwords(words, stopwords)
-    words = map(correction, words)
+    # words = map(correction, words)
     words = lemmatize_with_pos(words)
     words = [w for w in words if len(w) > 2]
     return words
